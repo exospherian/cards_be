61fed76ef63bfa1d4e67769d060e0162cb274cf5
exospherian <hialuron92@gmail.com>
2023-10-26 21:41:39 +0400

    add jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './JWT/constants';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+    JwtModule.register({
+        global: true,
+        secret: jwtConstants.secret,
+        signOptions: { expiresIn: '1000s' },
+    }),],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,15 @@ import { UserService } from '../user/user.service';
 import { User } from '../user/entities';
 import { hashPass } from './hash';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService
+        ) {}
 
     async signIn(username: string, password: string): Promise<any> {
 
@@ -25,7 +29,13 @@ export class AuthService {
             throw new UnauthorizedException("comparison failed");
         }
 
-        return user;
+        const payload = { sub: user.id, username: user.username };
+        let accessToken = {
+            access_token: await this.jwtService.signAsync(payload),
+        }
+        console.log(accessToken); 
+
+        return accessToken;
     }
 
     async signUp(username: string, password: string): Promise<User> {
